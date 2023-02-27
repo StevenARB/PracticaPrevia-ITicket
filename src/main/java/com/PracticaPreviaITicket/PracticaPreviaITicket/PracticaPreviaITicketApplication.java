70a4ec949ce5533fe6424121c4ad5b8dd0838b54
StevenARB <92124639+StevenARB@users.noreply.github.com>
2023-02-26 22:04:33 -0600

    Initial commit

---
+++ src/main/java/com/PracticaPreviaITicket/PracticaPreviaITicket/PracticaPreviaITicketApplication.java
@@ -0,0 +1,13 @@
+package com.PracticaPreviaITicket.PracticaPreviaITicket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticaPreviaITicketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticaPreviaITicketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/PracticaPreviaITicket/PracticaPreviaITicket/PracticaPreviaITicketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PracticaPreviaITicket.PracticaPreviaITicket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticaPreviaITicketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
